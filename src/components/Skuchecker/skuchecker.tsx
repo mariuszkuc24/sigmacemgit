64760e9ae85c645121b45d6358e0b0aef6800909
Jarowit1995 <62665045+Jarowit1995@users.noreply.github.com>
2022-12-09 09:38:33 +0100

    Update skuchecker.tsx

--- src/components/Skuchecker/skuchecker.tsx
+++ src/components/Skuchecker/skuchecker.tsx
@@ -1,70 +1,68 @@
-import React from 'react';
-import { useState } from 'react';
-import './skuchecker.scss';
-
-const Skuchecker = () => {
-  let shopSection: string = "";
-  let shopSectors: any = [];
-  const [value, setValue] = useState('');
-  const showSectors = () => {
-    const firstNumAtEnteredSku = parseInt(value.charAt(0));
-    console.log("len:" + value.length);
-    if (value.length === 4 || value.length === 5) {
-      console.log(firstNumAtEnteredSku);
-      switch (firstNumAtEnteredSku) {
-        case 1:
-          shopSection = "Metalowy";
-          shopSectors = ["Techniczny", "Remontujesz"];
-          break;
-        case 2:
-          shopSection = "Narzędzia";
-          shopSectors = ["Techniczny", "Remontujesz"];
-          break;
-        case 3:
-          shopSection = "Drewno";
-          shopSectors = ["Budujesz"];
-          break;
-        case 4:
-          shopSection = "Pokrycia podłogowe";
-          shopSectors = ["Urządzasz"];
-          break;
-        case 5:
-          shopSection = "Sanitarny";
-          shopSectors = ["Techniczny", "Remontujesz", "Urządzasz"];
-          break;
-        case 6:
-          shopSection = "Ogród";
-          shopSectors = ["Ogród"];
-          break;
-        case 7:
-          shopSection = "Dekorujesz";
-          shopSectors = ["Dekoracje"];
-          break;
-        case 8:
-          shopSection = "Elektryczny";
-          shopSectors = ["Metalowy", "Remontujesz"];
-          break;
-        case 9:
-          shopSection = "Budowlany";
-          shopSectors = ["Budujesz"];
-          break;
-        default:
-          shopSection = "Nie ta takiego działu, spróbuj ponownie!";
-          shopSectors = ["Nie ta takiego sektoru, spróbuj ponownie!"];
-          break;
-      }
+import React from "react";
+import { useState } from "react";
+import "./skuchecker.scss";
+const ShowShopSection = ({ inputed }: any) => {
+  let shopSection: any;
+  let shopSectors: any=[];
+  const firstNumAtEnteredSku = parseInt(inputed.charAt(0));
+ 
+    switch (firstNumAtEnteredSku) {
+      case 1:
+        shopSection = "Metalowy";
+        shopSectors = ["Techniczny", "Remontujesz"];
+        break;
+      case 2:
+        shopSection = "Narzędzia";
+        shopSectors = ["Techniczny", "Remontujesz"];
+        break;
+      case 3:
+        shopSection = "Drewno";
+        shopSectors = ["Budujesz"];
+        break;
+      case 4:
+        shopSection = "Pokrycia podłogowe";
+        shopSectors = ["Urządzasz"];
+        break;
+      case 5:
+        shopSection = "Sanitarny";
+        shopSectors = ["Techniczny", "Remontujesz", "Urządzasz"];
+        break;
+      case 6:
+        shopSection = "Ogród";
+        shopSectors = ["Ogród"];
+        break;
+      case 7:
+        shopSection = "Dekorujesz";
+        shopSectors = ["Dekoracje"];
+        break;
+      case 8:
+        shopSection = "Elektryczny";
+        shopSectors = ["Metalowy", "Remontujesz"];
+        break;
+      case 9:
+        shopSection = "Budowlany";
+        shopSectors = ["Budujesz"];
+        break;
+      default:
+        shopSection = "Nie ta takiego działu, spróbuj ponownie!";
+        shopSectors = ["Nie ta takiego sektoru, spróbuj ponownie!"];
+        break;
     }
-    console.log(shopSection);
+  return (
+    <div id="list-info">
+      <b>Dział:</b>
+      <ul id="list-section">{shopSection}</ul>
+      <b>Sektor/Sektory:</b>
+      <ul id="list-sectors">{shopSectors.map((shopSector: any)=>(<li key={shopSector}>{shopSector}</li>))}</ul>
+    </div>
+  );
+};
+const Skuchecker = () => {
+  const [value, setValue] = useState("");
 
-  }
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
-    showSectors();
   };
-
-  // console.log(value);
-  // console.log(typeof value);
-  // console.log(Number(value));
 
   return (
     <section className="sku-checker-container">
@@ -73,7 +71,10 @@ const Skuchecker = () => {
       </div>
 
       <div className="sku-checker-input-panel">
-        <h4>Podaj numer SKU by sprawdzić z którego działu, lub sektora pochodzi produkt: </h4>
+        <h4>
+          Podaj numer SKU by sprawdzić z którego działu, lub sektora pochodzi
+          produkt:{" "}
+        </h4>
         <label htmlFor="inp" className="inp">
           <input
             type="text"
@@ -91,17 +92,10 @@ const Skuchecker = () => {
           <h3>Działy i sektory:</h3>
         </div>
         <div className="sku-checker-output-panel-content">
-          <span>Numer SKU: <em id="log-number">{value}</em></span>
-          <div id="list-info">
-            <b>Dział:</b>
-            <ul id="list-section">
-              {shopSection}
-            </ul>
-            <b>Sektor/Sektory:</b>
-            <ul id="list-sectors">
-              {shopSectors}
-            </ul>
-          </div>
+          <span>
+            Numer SKU: <em id="log-number">{value}</em>
+          </span>
+          <ShowShopSection inputed={value} />
         </div>
       </div>
     </section>
